0b18b8b4b1c70ef2fbe9c1b4c720d30e0abd41e8
Pablo  Chaves <pablo161096@gmail.com>
2021-09-29 01:13:41 -0500

    deleted files

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const qrcode = require('qrcode-terminal')
 const ora = require('ora')
 const chalk = require('chalk')
 
-const listenMessage = require('./src/listenMessage')
+const listenMessage = require('./services/listenMessage')
 
 const SESSION_FILE_PATH = './private/session.json'
 let client
